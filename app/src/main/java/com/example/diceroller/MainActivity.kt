13874744075c7ade42c4936327cc62ef3c5c8e3e
Mihcchan <48868786+mihcchan@users.noreply.github.com>
2021-03-17 11:53:15 -0300

    dado com imagens

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -14,19 +14,40 @@ class MainActivity : AppCompatActivity() {
         rollButton.setOnClickListener {
             rollDice()
         }
+        //Rola o dado quando abre o app
+        rollDice()
     }
 
+    /**
+     * Rola o dado e atualiza a tela com o resultado
+     */
     private fun rollDice() {
+        //Cria um novo dado com 6 lados e rola o dado
         val dice = Dice(6)
-        val dice2 = Dice(6)
         val diceRoll = dice.roll()
-        val diceRoll2 = dice2.roll()
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
-        val resultTextView2: TextView = findViewById(R.id.textView2)
-        resultTextView2.text = diceRoll2.toString()
+
+        //Encontra a ImageView no layout
+        val diceImage: ImageView = findViewById(R.id.imageView)
+
+        //Determina qual drawable resource ID será utilizado baseado no dado rolado
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        //Atualiza a ImageView com o id correto
+        diceImage.setImageResource(drawableResource)
+
+        //Atualiza a descrição da imagem com o numero do dado rolado
+        diceImage.contentDescription = diceRoll.toString()
     }
 }
+
+
 
 class Dice(private val numSides: Int) {
 
